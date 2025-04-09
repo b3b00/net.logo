53e11c4d200a0235bb641418f4577ac22def3c72
Olivier Duhart <olivier.duhart@sage.com>
2025-04-09 08:51:22 +0200

    cli initial

---
+++ src/net.logo.cli/Options.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace net.logo.cli;
+
+[Verb("compile", HelpText = "Check a logo file.")]
+public class CompileOptions
+{
+    [Value(0, Required = true, HelpText = "Path to the logo file.")]
+    public string LogoFilePath { get; set; }
+    
+    [Option('v', "verbose", Required = false, HelpText = "Enable verbose output.")]
+    public bool Verbose { get; set; } = false;
+}
+
+[Verb("generate", HelpText = "Generate a SVG from a logo file.")]
+public class GenerateOptions
+{
+    [Option('o', "output", Required = false, HelpText = "Output file name.")]
+    public string OutputFile { get; set; } = string.Empty;
+
+    [Option('v', "verbose", Required = false, HelpText = "Enable verbose output.")]
+    public bool Verbose { get; set; } = false;
+    
+    [Value(0, Required = true, HelpText = "Path to the logo file.")]
+    public string LogoFilePath { get; set; }
+    
+}
+
--- src/net.logo.cli/Program.cs
+++ src/net.logo.cli/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace net.logo.cli;
+﻿using CommandLine;
+
+namespace net.logo.cli;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static int Main(string[] args)
     {
-        
+        return CommandLine.Parser.Default.ParseArguments<CompileOptions, GenerateOptions>(args)
+            .MapResult(
+                (CompileOptions opts) => Compile(opts),
+                (GenerateOptions opts) => Generate(opts),
+                errors =>
+                {
+                    foreach (Error error in errors)
+                    {
+                        Console.WriteLine(error.ToString());
+                    }
+                   
+                    return 1;
+                });
+    }
+
+    private static int Generate(GenerateOptions opts)
+    {
+        Console.WriteLine($"generate logo file {opts.LogoFilePath} to {opts.OutputFile}");
+        return 0;
+    }
+
+    private static int Compile(CompileOptions opts)
+    {
+        Console.WriteLine($"check logo file {opts.LogoFilePath}");
+        return 0;
     }
 }
